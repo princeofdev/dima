4c771e73d826527a32a63be9ad87a12298da0970
chargecyber <edomkyc777@gmail.com>
2023-05-16 09:23:47 +0900

    css optimize

--- assets/js/core.js
+++ assets/js/core.js
@@ -34,7 +34,6 @@ var Mi = {
             self.masonry();
             self.ajaxLoader();
             self.mobileNav();
-            self.forms();
 
         },
         mobileDetector: function () {
@@ -251,88 +250,6 @@ var Mi = {
                 $('body').toggleClass('mobile-nav-open');
                 return false;
             });
-        },
-        forms: function() {
-
-            var $formAlert, $formError;
-
-            // Basic Form 
-
-            var $basicForm  = $('.basic-form');
-            $basicForm.validate({
-                errorPlacement: function(error, element) { }
-            });
-            $basicForm.submit(function() {
-                $formAlert = $(this).find('.form-alert');
-                $formError = $(this).find('.form-error');
-                if(!$basicForm.valid()) $formError.show();
-            });
-
-            // Contact Form
-
-            var $contactForm  = $('#contact-form');
-    
-            $contactForm.validate({
-                errorElement: 'span',
-                errorContainer: $contactForm.find('.form-error'),
-                errorLabelContainer: $contactForm.find('.form-error ul'),
-                wrapper: "li",
-                rules: {
-                    name: {
-                        required    : true,
-                        minlength   : 2
-                    },
-                    email: {
-                        required    : true,
-                        email       : true
-                    },
-                    message: {
-                        required    : true,
-                        minlength   : 10
-                    }
-                },
-                messages: {
-                    name: {
-                        required    : "Please enter your name.",
-                        minlength   : "Your name needs to be at least 2 characters"
-                    },
-                    email: {
-                        required    : "Please enter your email address.",
-                        minlength   : "You entered an invalid email address."
-                    },
-                    message: {
-                        required    : "Please enter a message.",
-                        minlength   : "Your message needs to be at least 10 characters"
-                    }
-                }
-            });
-        
-            $contactForm.submit(function() {
-                $formAlert = $(this).find('.form-alert');
-                $formError = $(this).find('.form-error');
-                var response;
-                $formAlert.hide().html();
-                if ($contactForm.valid()){
-                    $.ajax({
-                        type: "POST",
-                        url: "assets/php/contact-form.php",
-                        data: $(this).serialize(),
-                        success: function(msg) {
-                            if (msg === 'SEND') {
-                                response = '<div class="alert alert-success">Done! Thank you for your message - You will get an answer as soon as possible.';
-                            }
-                            else {
-                                response = '<div class="alert alert-danger">Ooops... It seems that we have a problem.';
-                            }
-                            $formAlert.html(response);
-                            $formAlert.show();
-                        }
-                     });
-                    return false;
-                }
-                return false;
-            });
-
         }
     },
     Components: {
@@ -345,7 +262,6 @@ var Mi = {
             this.tooltip(); 
             this.popover();
             this.messenger();
-            this.videoPlayer();
             this.navToggleable();
             this.navFilter();
 
@@ -435,16 +351,6 @@ var Mi = {
         },
         popover: function() {
             $("[rel='popover']").popover();
-        },
-        videoPlayer: function() {
-            var $videoPlayer = $('.video-player');
-            if($videoPlayer) {
-                $videoPlayer.YTPlayer();
-            }
-            if(trueMobile && $videoPlayer.hasClass('bg-video')) {
-                $videoPlayer.prev('.bg-video-placeholder').show();
-                $videoPlayer.remove()
-            } 
         },
         messenger: function() {
             $('[data-target="messenger"]').on('click',function(){
